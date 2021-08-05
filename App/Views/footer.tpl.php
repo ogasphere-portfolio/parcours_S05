f4a9b2819919c057b304b376c611a5f53fa60773
Olivier <olivier@ogasphere.com>
2021-08-05 09:15:58 +0200

    modif footer

--- App/Views/footer.tpl.php
+++ App/Views/footer.tpl.php
@@ -1,28 +1,20 @@
 <footer class="main-footer">
-   
-    <div id="footer-copyright" class="py-3">
-      <div class="container">
-        <div class="row align-items-center">
-          <div class="col-md-3 text-center text-md-left">
-            <p class="mb-md-0">Tous droits réservés<br>© <?= date('Y') ?> Sonic</p>
-          </div>
-          <div class="col-md-6 text-center">
-            <p class="mb-md-0"><a class="text-white" href="<?= $absoluteURL ?>/mentions-legales">Mentions légales</a><br><a class="text-white" href="#">Conditions générales de vente</a></p>
-          </div>
-        
-          <div class="col-md-3">
-            
-          </div>
+  <div id="footer-copyright" class="py-3">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-12 text-center text-md-center">
+          <p class="mb-md-0">Tous droits réservés<br>© <?= date('Y') ?> Sonic</p>
         </div>
       </div>
     </div>
-  </footer>
+  </div>
+</footer>
 
-  <!-- JS -->
-  <script src="<?= $absoluteURL ?>/assets/js/jquery.min.js"></script>
-  <script src="<?= $absoluteURL ?>/assets/js/popper.min.js"></script>
-  <script src="<?= $absoluteURL ?>/assets/js/bootstrap.min.js"></script>
-  <script src="<?= $absoluteURL ?>/assets/js/app.js"></script>
+<!-- JS -->
+<script src="<?= $absoluteURL ?>/assets/js/jquery.min.js"></script>
+<script src="<?= $absoluteURL ?>/assets/js/popper.min.js"></script>
+<script src="<?= $absoluteURL ?>/assets/js/bootstrap.min.js"></script>
+<script src="<?= $absoluteURL ?>/assets/js/app.js"></script>
 </body>
 
 </html>
